4c505dd96c7ce7ec0e05812a381cccccabb0fedc
bvincent1 <benjc.vincent@gmail.com>
2017-03-29 20:21:52 -0600

    Add first task code

--- task_1.py
+++ task_1.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
 
+from sh import git
+from random import random
+
+def main():
+    git.checkout("task_1")
+
+    f = open("file1.txt", "w")
+    f.write("here is a file")
+    f.close()
+
+    git.add("file1.txt")
+    git.commit('-m "commited new text file1"')
+    git.push()
+
+    git.checkout("-")
+
+
+if __name__ == '__main__':
+    main()
